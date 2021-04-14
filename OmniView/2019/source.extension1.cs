d2a95dc93c163c0c90c2a4b107342902d1431c7d
Ramon <rfmendesbrazil@gmail.com>
2021-04-14 18:48:40 -0300

    2019

--- OmniView/2019/source.extension1.cs
+++ OmniView/2019/source.extension1.cs
@@ -7,11 +7,11 @@ namespace OmniView._2019
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "OmniView.MISoftware.e3a6c7fb-0320-4655-9a97-afdf58c57fae";
-        public const string Name = "OmniView for VS2017";
+        public const string Id = "OmniView.MISoftware.e3a6c7fb-0320-4655-9a97-afdf58c57fa3";
+        public const string Name = "OmniView for VS2019";
         public const string Description = @"Provides a previewer margin to the HTML editor, allowing you to instantly see the output of Sciter HTML page";
         public const string Language = "en-US";
-        public const string Version = "2.16";
+        public const string Version = "2.17";
         public const string Author = "MI Software";
         public const string Tags = "Sciter, HTML, CSS";
     }
